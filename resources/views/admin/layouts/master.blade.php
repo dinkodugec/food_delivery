45cf641c320c70690b108a6eae99cbdecafe6299
Dinko <dinko.dugec@gmail.com>
2024-09-10 22:28:15 +0200

    admin page setup

---
+++ resources/views/admin/layouts/master.blade.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" name="viewport">
+  <title>General Dashboard &mdash; Stisla</title>
+
+  <!-- General CSS Files -->
+  <link rel="stylesheet" href="{{ asset('admin/assets/modules/bootstrap/css/bootstrap.min.css')}}">
+  <link rel="stylesheet" href="{{ asset('admin/assets/modules/fontawesome/css/all.min.css')}}">
+
+  <!-- CSS Libraries -->
+
+
+  <!-- Template CSS -->
+  <link rel="stylesheet" href="{{ asset('admin/assets/css/style.css')}}">
+  <link rel="stylesheet" href="{{ asset('admin/assets/css/components.css')}}">n/
+<!-- Start GA -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-94034622-3"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-94034622-3');
+</script>
+<!-- /END GA --></head>
+
+<body>
+  <div id="app">
+    <div class="main-wrapper main-wrapper-1">
+      <div class="navbar-bg"></div>
+
+      @include('admin.layouts.sidebar')
+
+      <!-- Main Content -->
+      <div class="main-content">
+      @yield('content')
+      </div>
+      <footer class="main-footer">
+        <div class="footer-left">
+          Copyright &copy; 2024 <div class="bullet"></div> Design By <a href="https://nauval.in/">Dinko Dugeč</a>
+        </div>
+        <div class="footer-right">
+
+        </div>
+      </footer>
+    </div>
+  </div>
+
+  <!-- General JS Scripts -->
+  <script src="assets/modules/jquery.min.js"></script>
+  <script src="assets/modules/popper.js"></script>
+  <script src="assets/modules/tooltip.js"></script>
+  <script src="assets/modules/bootstrap/js/bootstrap.min.js"></script>
+  <script src="assets/modules/nicescroll/jquery.nicescroll.min.js"></script>
+  <script src="assets/js/stisla.js"></script>
+
+  <!-- JS Libraies -->
+  
+
+  <!-- Template JS File -->
+  <script src="{{asset ('admin/assets/js/scripts.js')}}"></script>
+  <script src="{{asset ('admin/assets/js/custom.js')}}"></script>
+</body>
+</html>
