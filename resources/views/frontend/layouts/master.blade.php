342249b868664068660e92dc72f4852b09388d7b
Dinko <dinko.dugec@gmail.com>
2024-09-20 22:37:14 +0200

    toaster package

--- resources/views/frontend/layouts/master.blade.php
+++ resources/views/frontend/layouts/master.blade.php
@@ -15,7 +15,7 @@
     <link rel="stylesheet" href="{{ asset('frontend/css/venobox.min.css') }}">
     <link rel="stylesheet" href="{{ asset('frontend/css/animate.css') }}">
     <link rel="stylesheet" href="{{ asset('frontend/css/jquery.exzoom.css') }}">
-
+    <link rel="stylesheet" href="{{ asset('frontend/css/toastr.min.css') }}">
     <link rel="stylesheet" href="{{ asset('frontend/css/style.css') }}">
     <link rel="stylesheet" href="{{ asset('frontend/css/responsive.css') }}">
     <!-- <link rel="stylesheet" href="css/rtl.css"> -->
@@ -84,34 +84,47 @@
     ==============================-->
 
 
-    <!--jquery library js-->
-    <script src="js/jquery-3.6.0.min.js"></script>
-    <!--bootstrap js-->
-    <script src="js/bootstrap.bundle.min.js"></script>
-    <!--font-awesome js-->
-    <script src="js/Font-Awesome.js"></script>
-    <!-- slick slider -->
-    <script src="js/slick.min.js"></script>
-    <!-- isotop js -->
-    <script src="js/isotope.pkgd.min.js"></script>
-    <!-- simplyCountdownjs -->
-    <script src="js/simplyCountdown.js"></script>
-    <!-- counter up js -->
-    <script src="js/jquery.waypoints.min.js"></script>
-    <script src="js/jquery.countup.min.js"></script>
-    <!-- nice select js -->
-    <script src="js/jquery.nice-select.min.js"></script>
-    <!-- venobox js -->
-    <script src="js/venobox.min.js"></script>
-    <!-- sticky sidebar js -->
-    <script src="js/sticky_sidebar.js"></script>
-    <!-- wow js -->
-    <script src="js/wow.min.js"></script>
-    <!-- ex zoom js -->
-    <script src="js/jquery.exzoom.js"></script>
+     <!--jquery library js-->
+     <script src="{{ asset('frontend/js/jquery-3.6.0.min.js') }}"></script>
+     <!--bootstrap js-->
+     <script src="{{ asset('frontend/js/bootstrap.bundle.min.js') }}"></script>
+     <!--font-awesome js-->
+     <script src="{{ asset('frontend/js/Font-Awesome.js') }}"></script>
+     <!-- slick slider -->
+     <script src="{{ asset('frontend/js/slick.min.js') }}"></script>
+     <!-- isotop js -->
+     <script src="{{ asset('frontend/js/isotope.pkgd.min.js') }}"></script>
+     <!-- simplyCountdownjs -->
+     <script src="{{ asset('frontend/js/simplyCountdown.js') }}"></script>
+     <!-- counter up js -->
+     <script src="{{ asset('frontend/js/jquery.waypoints.min.js') }}"></script>
+     <script src="{{ asset('frontend/js/jquery.countup.min.js') }}"></script>
+     <!-- nice select js -->
+     <script src="{{ asset('frontend/js/jquery.nice-select.min.js') }}"></script>
+     <!-- venobox js -->
+     <script src="{{ asset('frontend/js/venobox.min.js') }}"></script>
+     <!-- sticky sidebar js -->
+     <script src="{{ asset('frontend/js/sticky_sidebar.js') }}"></script>
+     <!-- wow js -->
+     <script src="{{ asset('frontend/js/wow.min.js') }}"></script>
+     <!-- ex zoom js -->
+     <script src="{{ asset('frontend/js/jquery.exzoom.js') }}"></script>
 
-    <!--main/custom js-->
-    <script src="js/main.js"></script>
+     <script src="{{ asset('frontend/js/toastr.min.js') }}"></script>
+
+     <!--main/custom js-->
+     <script src="{{ asset('frontend/js/main.js') }}"></script>
+
+    <script>
+        toastr.options.progressBar = true;
+
+        @if ($errors->any())
+            @foreach ($errors->all() as $error)
+                toastr.error("{{ $error }}")
+            @endforeach
+        @endif
+    </script>
+
 
 </body>
 
