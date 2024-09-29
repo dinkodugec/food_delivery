799cf9ebc1e39dcfe15a6a012bf6120934354fab
Dinko <dinko.dugec@gmail.com>
2024-09-29 11:41:55 +0200

    admin login page

--- routes/admin.php
+++ routes/admin.php
@@ -4,8 +4,15 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\AdminDashboardController;
+use App\Http\Controllers\Admin\ProfileController;
 
      Route::group(['prefix' => 'admin', 'as' => 'admin.'], function(){
      Route::get('dashboard', [AdminDashboardController::class, 'index'])->name('dashboard');
+
+        /** Profile Routes */
+    Route::get('profile', [ProfileController::class, 'index'])->name('profile');
+
+    Route::put('profile', [ProfileController::class, 'updateProfile'])->name('profile.update');
+  
 
 });
