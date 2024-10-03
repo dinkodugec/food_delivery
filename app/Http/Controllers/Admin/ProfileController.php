4dce5a123a19d067d93a5c9d8aa0061ef48428ff
Dinko <dinko.dugec@gmail.com>
2024-10-03 18:40:38 +0200

    admin avatar update - image uploaded

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -5,25 +5,39 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Admin\ProfilePasswordUpdateRequest;
 use App\Http\Requests\Admin\ProfileUpdateRequest;
+use App\Traits\FileUploadTrait;
 use Illuminate\Contracts\View\View;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Auth;
 
+
 class ProfileController extends Controller
 {
-    function index() : View {
+    use FileUploadTrait;
+
+
+    function index() : View
+    {
         return view('admin.profile.index');
     }
 
+
+
     function updateProfile(ProfileUpdateRequest $request) : RedirectResponse
     {
+
        /*  dd($request->all()); */
+
 
         $user = Auth::user();
 
+        $imagePath = $this->uploadImage($request, 'avatar');
+      
+
         $user->name = $request->name;
         $user->email = $request->email;
+        $user->avatar = isset($imagePath) ? $imagePath : $user->avatar;
         $user->save();
 
         toastr('Updated Successfully!', 'success');
