9594b8564836f8924bf8708aac0abacc07ae55fc
Dinko <dinko.dugec@gmail.com>
2024-10-13 18:59:43 +0200

    create slider

--- app/Http/Controllers/Admin/SliderController.php
+++ app/Http/Controllers/Admin/SliderController.php
@@ -4,10 +4,16 @@ namespace App\Http\Controllers\Admin;
 
 use App\DataTables\SliderDataTable;
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Admin\SliderCreateRequest;
+use App\Models\Slider;
+use App\Traits\FileUploadTrait;
+use Illuminate\Contracts\View\View;
 use Illuminate\Http\Request;
 
 class SliderController extends Controller
 {
+
+    use FileUploadTrait;
     /**
      * Display a listing of the resource.
      */
@@ -16,20 +22,38 @@ class SliderController extends Controller
         return $dataTable->render('admin.slider.index');
     }
 
+
+
     /**
      * Show the form for creating a new resource.
      */
-    public function create()
+    public function create() : View
     {
-        //
+        return view('admin.slider.create');
     }
+
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(SliderCreateRequest $request)
     {
-        //
+            /** Handle image upload */
+            $imagePath = $this->uploadImage($request, 'image');
+
+            $slider = new Slider();
+            $slider->image = $imagePath;
+            $slider->offer = $request->offer;
+            $slider->title = $request->title;
+            $slider->sub_title = $request->sub_title;
+            $slider->short_description = $request->short_description;
+            $slider->button_link = $request->button_link;
+            $slider->status = $request->status;
+            $slider->save();
+
+            toastr()->success('Created Successfully');
+
+            return to_route('admin.slider.index');
     }
 
     /**
