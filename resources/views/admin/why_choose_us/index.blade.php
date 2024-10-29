99e9e4fd0ac23a8edbfa7b39beb77b7ed0d34100
Dinko <dinko.dugec@gmail.com>
2024-10-29 23:14:36 +0100

    Showing Dynamic Titles in Title Form

--- resources/views/admin/why_choose_us/index.blade.php
+++ resources/views/admin/why_choose_us/index.blade.php
@@ -13,8 +13,22 @@
                         <div class="accordion-header collapsed bg-primary text-light p-3 " role="button"
                             data-toggle="collapse" data-target="#panel-body-1" aria-expanded="false">
                             <h4>Why Choose Us Section Titles..</h4>
-                        </div>
-                        <div class="accordion-body collapse" id="panel-body-1" data-parent="#accordion" style="">
+                                <form>
+                                <div class="form-group">
+                                    <label for="">Top Title</label>
+                                    <input type="text" class="form-control" name="why_choose_top_title" value="{{ $titles['why_choose_top_title'] }}">
+                                </div>
+                                <div class="form-group">
+                                    <label for="">Main Title</label>
+                                    <input type="text" class="form-control" name="why_choose_main_title" value="{{ $titles['why_choose_main_title'] }}">
+                                </div>
+                                <div class="form-group">
+                                    <label for="">Sub Title</label>
+                                    <input type="text" class="form-control" name="why_choose_sub_title" value="{{ $titles['why_choose_sub_title'] }}">
+                                </div>
+                                <button type="submit" class="btn btn-primary">Save</button>
+                            </form>
+
                             <div class="form-group">
                                 <label for="">Top Title</label>
                                 <input type="text" class="form-control">
