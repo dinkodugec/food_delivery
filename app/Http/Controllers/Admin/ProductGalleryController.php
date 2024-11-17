1017165721bf3549d0a26b9198e466810d6df97d
Dinko <dinko.dugec@gmail.com>
2024-11-17 22:55:22 +0100

    multiple images for products

--- app/Http/Controllers/Admin/ProductGalleryController.php
+++ app/Http/Controllers/Admin/ProductGalleryController.php
@@ -3,17 +3,25 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Product;
+use App\Models\ProductGallery;
+use App\Traits\FileUploadTrait;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\View\View;
 
 class ProductGalleryController extends Controller
 {
+
+    use FileUploadTrait;
     /**
      * Display a listing of the resource.
      */
-    public function index() :View
+    public function index(string $productId) :View
     {
-        return view('admin.product.gallery.index');
+        $images = ProductGallery::where('product_id', $productId)->get();
+        $product = Product::findOrFail($productId);
+        return view('admin.product.gallery.index', compact('product', 'images'));
     }
 
     /**
@@ -27,9 +35,23 @@ class ProductGalleryController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(Request $request)  : RedirectResponse
     {
-        //
+        $request->validate([
+            'image' => ['required', 'image', 'max:3000'],
+            'product_id' => ['required', 'integer']
+        ]);
+
+        $imagePath = $this->uploadImage($request, 'image');
+
+        $gallery = new ProductGallery();
+        $gallery->product_id = $request->product_id;
+        $gallery->image = $imagePath;
+        $gallery->save();
+
+        toastr()->success('Created Successfully!');
+
+        return redirect()->back();
     }
 
     /**
@@ -59,8 +81,16 @@ class ProductGalleryController extends Controller
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(string $id) 
     {
-        //
+        try{
+            $image = ProductGallery::findOrFail($id);
+            $this->removeImage($image->image);
+            $image->delete();
+
+            return response(['status' => 'success', 'message' => 'Deleted Successfully!']);
+        }catch(\Exception $e){
+            return response(['status' => 'error', 'message' => 'something went wrong!']);
+        }
     }
 }
