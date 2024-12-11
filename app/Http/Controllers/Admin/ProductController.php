dd4f04c3b642e5f54d9bceb15fde9b66fca748d8
Dinko <dinko.dugec@gmail.com>
2024-12-11 22:19:28 +0100

    Showing Products - some bug with slug

--- app/Http/Controllers/Admin/ProductController.php
+++ app/Http/Controllers/Admin/ProductController.php
@@ -96,7 +96,7 @@ class ProductController extends Controller
 
                 $product->thumb_image = !empty($imagePath) ? $imagePath : $product->thumb_image;
                 $product->name = $request->name;
-                $product->slug = generateUniqueSlug('Product', $request->name);
+             /*    $product->slug = generateUniqueSlug('Product', $request->name); we do not need update slack */
                 $product->category_id = $request->category;
                 $product->price = $request->price;
                 $product->offer_price = $request->offer_price;
