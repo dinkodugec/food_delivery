e8912f45699f236e766c433ddfadbba9243f2c44
Dinko <dinko.dugec@gmail.com>
2024-12-23 19:44:17 +0100

    Add to Cart - Working with Add to Cart Feature (Part - 2)

--- app/Http/Controllers/Frontend/CartController.php
+++ app/Http/Controllers/Frontend/CartController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Frontend;
 
 use App\Http\Controllers\Controller;
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class CartController extends Controller
@@ -12,6 +13,26 @@ class CartController extends Controller
      */
     public function addToCart(Request $request)
      {
-        dd($request->all());
+       /*  dd($request->all());
+ */
+        $product = Product::with(['productSizes', 'productOptions'])->findOrFail($request->product_id);
+        $productSize = $product->productSizes->where('id', $request->product_size)->first();
+        $productOptions = $product->productOptions->whereIn('id', $request->product_option);
+        $options = [
+            'product_size' => [
+                'id' => $productSize->id,
+                'name' => $productSize->name,
+                'price' => $productSize->price
+            ],
+            'product_options' => []
+        ];
+        foreach($productOptions as $option) {
+            $options['product_options'][] =[
+                'id' => $option->id,
+                'name' => $option->name,
+                'price' => $option->price
+            ];
+        }
+        dd($options);
     }
 }
