96caadff2c206672df3f021320e05c6042bbc25a
Dinko <dinko.dugec@gmail.com>
2024-12-24 19:52:07 +0100

    Add to Cart - Adding loader at Add to Cart Button

--- resources/views/frontend/layouts/global-scripts.blade.php
+++ resources/views/frontend/layouts/global-scripts.blade.php
@@ -1,27 +1,26 @@
 <script>
 
     /** Loard product modal**/
-    function loadProductModal(productId)
-    {
+    function loadProductModal(productId){
         $.ajax({
             method: 'GET',
             url: '{{ route("load-product-modal", ":productId") }}'.replace(':productId', productId),
-
             beforeSend: function(){
-            $('.overlay').addClass('active');
-        },
-            success: function(response){
+                $('.overlay-container').removeClass('d-none');
+                $('.overlay').addClass('active');
 
+            },
+            success: function(response){
                 $(".load_product_modal_body").html(response);
                 $('#cartModal').modal('show');
-
             },
             error: function(xhr, status, error){
                 console.error(error);
             },
-
             complete: function(){
                 $('.overlay').removeClass('active');
+                $('.overlay-container').addClass('d-none');
+
             }
         })
     }
--- resources/views/frontend/layouts/master.blade.php
+++ resources/views/frontend/layouts/master.blade.php
@@ -27,7 +27,7 @@
 
 <body>
 
-    <div class="overlay-container">
+    <div class="overlay-container d-none">
         <div class="overlay">
             <span class="loader"></span>
         </div>
