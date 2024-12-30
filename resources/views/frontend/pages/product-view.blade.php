e8e8857ad9d92021cf1304c2d8693429b4f96c23
dinkodugec <dinko.dugec@gmail.com>
2024-12-30 22:56:34 +0100

    View Page Add to Cart - Working with Quantity Increment

--- resources/views/frontend/pages/product-view.blade.php
+++ resources/views/frontend/pages/product-view.blade.php
@@ -75,7 +75,7 @@
                         <form action="">
                             @csrf
                             <input type="hidden" name="base_price" class="v_base_price"
-                                value="{{ $product->offer_price > 0 ? $product->offer_price : $product_price }}">
+                                value="{{ $product->offer_price > 0 ? $product->offer_price : $product->price }}">
 
                             @if ($product->productSizes()->exists())
                                 <div class="details_size">
@@ -87,7 +87,7 @@
                                                 name="flexRadioDefault" id="size-{{ $productSize->id }}"
                                                 data-price="{{ $productSize->price }}">
                                             <label class="form-check-label" for="size-{{ $productSize->id }}">
-                                                {{ $productSize->name }} <span>+ ${{ $productSize->price }}</span>
+                                                {{ $productSize->name }} <span>+ {{ currencyPosition($productSize->price) }}</span>
                                             </label>
                                         </div>
                                     @endforeach
@@ -104,7 +104,7 @@
                                                 id="option-{{ $productOption->id }}"
                                                 data-price="{{ $productOption->price }}">
                                             <label class="form-check-label" for="option-{{ $productOption->id }}">
-                                                {{ $productOption->name }} <span>+ ${{ $productOption->price }}</span>
+                                                {{ $productOption->name }} <span>+ {{ currencyPosition($productOption->price) }}</span>
                                             </label>
                                         </div>
                                     @endforeach
@@ -115,11 +115,11 @@
                                 <h5>select quentity</h5>
                                 <div class="quentity_btn_area d-flex flex-wrapa align-items-center">
                                     <div class="quentity_btn">
-                                        <button class="btn btn-danger"><i class="fal fa-minus"></i></button>
-                                        <input type="text" placeholder="1">
-                                        <button class="btn btn-success"><i class="fal fa-plus"></i></button>
+                                        <button class="btn btn-danger v_decrement"><i class="fal fa-minus"></i></button>
+                                        <input type="text" name="qty" placeholder="1" value="1" readonly id="v_quantity">
+                                        <button class="btn btn-success v_increment"><i class="fal fa-plus"></i></button>
                                     </div>
-                                    <h3 id="v_total_price">$320.00</h3>
+                                    <h3 id="v_total_price">{{ $product->offer_price > 0 ? currencyPosition($product->offer_price) : currencyPosition($product->price) }}</h3>
                                 </div>
                             </div>
                         </form>
@@ -410,6 +410,10 @@
 @push('scripts')
     <script>
         $(document).ready(function() {
+            $('.v_product_size').prop('checked', false);
+            $('.v_product_option').prop('checked', false);
+            $('#v_quantity').val(1);
+
             $('.v_product_size').on('change', function() {
                 v_updateTotalPrice()
             });
@@ -418,13 +422,34 @@
                 v_updateTotalPrice()
             });
 
+            // Event handlers for increment and decrement buttons
+            $('.v_increment').on('click', function(e) {
+                e.preventDefault()
+
+                let quantity = $('#v_quantity');
+                let currentQuantity = parseFloat(quantity.val());
+                quantity.val(currentQuantity + 1);
+                v_updateTotalPrice()
+            })
+
+            $('.v_decrement').on('click', function(e) {
+                e.preventDefault()
+
+                let quantity = $('#v_quantity');
+                let currentQuantity = parseFloat(quantity.val());
+                if (currentQuantity > 1) {
+                    quantity.val(currentQuantity - 1);
+                    v_updateTotalPrice()
+                }
+            })
+
 
             // Function to update the total price base on seelected options
             function v_updateTotalPrice() {
                 let basePrice = parseFloat($('.v_base_price').val());
                 let selectedSizePrice = 0;
                 let selectedOptionsPrice = 0;
-                let quantity = parseFloat($('#quantity').val());
+                let quantity = parseFloat($('#v_quantity').val());
 
                 // Calculate the selected size price
                 let selectedSize = $('.v_product_size:checked');
@@ -439,7 +464,7 @@
                 })
 
                 // Calculate the total price
-                let totalPrice = (basePrice + selectedSizePrice + selectedOptionsPrice);
+                let totalPrice = (basePrice + selectedSizePrice + selectedOptionsPrice) * quantity;
                 $('#v_total_price').text("{{ config('settings.site_currency_icon') }}" + totalPrice);
 
             }
