5879e5d954aebefb32bbde4a30524f538e649e24
dinkodugec <dinko.dugec@gmail.com>
2025-01-01 13:46:03 +0100

    View Page Add to Cart - Adding Add to Cart Feature

--- resources/views/frontend/layouts/master.blade.php
+++ resources/views/frontend/layouts/master.blade.php
@@ -34,6 +34,27 @@
        {{--  {{ Cart::destroy(); }} cart destroy --}}
     </div>
 
+       <!--=============================
+        Cart Popup Modal Start
+    ==============================-->
+
+    <div class="fp__cart_popup">
+        <div class="modal fade" id="cartModal" tabindex="-1" aria-hidden="true">
+            <div class="modal-dialog modal-dialog-centered">
+                <div class="modal-content">
+                    <div class="modal-body load_product_modal_body">
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+       <!--=============================
+        Cart Popup Modal End
+    ==============================-->
+
+
     <!--=============================
         TOPBAR START
     ==============================-->
