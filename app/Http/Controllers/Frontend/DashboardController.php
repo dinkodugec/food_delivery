3a76866e865c8586b70f592762c4701c4653efca
dinkodugec <dinko.dugec@gmail.com>
2025-02-08 21:05:47 +0100

    create addres for user in dashboard

--- app/Http/Controllers/Frontend/DashboardController.php
+++ app/Http/Controllers/Frontend/DashboardController.php
@@ -4,12 +4,21 @@ namespace App\Http\Controllers\Frontend;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Contracts\View\View;
+use App\Models\DeliveryArea;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
     public function index() : View
     {
-        return view('frontend.dashboard.index');
+        $deliveryAreas = DeliveryArea::where('status', 1)->get();
+        return view('frontend.dashboard.index', compact('deliveryAreas'));
     }
+
+    
+    function createAddress(Request $request)
+    {
+        dd($request->all());
+    }
+
 }
--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Admin\AdminAuthController;
 use App\Http\Controllers\Frontend\DashboardController;
 use App\Http\Controllers\Frontend\CartController;
 use App\Http\Controllers\Frontend\ProfileController;
+use SebastianBergmann\CodeCoverage\Report\Html\Dashboard;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -78,3 +79,5 @@ Route::get('/cart-destroy', [CartController::class, 'cartDestroy'])->name('cart.
 Route::post('/apply-coupon', [FrontendController::class, 'applyCoupon'])->name('apply-coupon');
 
 Route::get('/destroy-coupon', [FrontendController::class, 'destroyCoupon'])->name('destroy-coupon');
+
+Route::post('address', [DashboardController::class, 'createAddress'])->name('address.store');
