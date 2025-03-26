204a587657164751f63754c1d8d4b322dabfd5b5
dinkodugec <dinko.dugec@gmail.com>
2025-03-26 23:34:28 +0100

    Paypal Integration - Working with Paypal Integration (Part - 2)

--- app/Http/Controllers/Frontend/PaymentController.php
+++ app/Http/Controllers/Frontend/PaymentController.php
@@ -3,17 +3,16 @@
 namespace App\Http\Controllers\Frontend;
 
 use App\Http\Controllers\Controller;
-use App\Models\Order;
 use App\Services\OrderService;
+use Illuminate\Http\Request;
 use Illuminate\Validation\ValidationException;
 use Illuminate\View\View;
-use Illuminate\Http\Request;
 
 class PaymentController extends Controller
 {
-    function index() : View
+    function index(): View
     {
-        if(!session()->has('delivery_fee') || !session()->has('address')) {
+        if (!session()->has('delivery_fee') || !session()->has('address')) {
             throw ValidationException::withMessages(['Something went wrong']);
         }
 
@@ -29,42 +28,63 @@ class PaymentController extends Controller
         ));
     }
 
-    function makePayment(Request $request, OrderService $orderService )
-     {
-
+    function makePayment(Request $request, OrderService $orderService)
+    {
         $request->validate([
             'payment_gateway' => ['required', 'string', 'in:paypal']
         ]);
 
+        /** Create Order */
+        if ($orderService->createOrder()) {
+            // redirect user to the payment host
+            switch ($request->payment_gateway) {
+                case 'paypal':
+                    return response(['redirect_url' => route('paypal.payment')]);
+                    break;
 
-             /** Create Order */
-             if($orderService->createOrder()){
-                // redirect user to the payment host
-                switch ($request->payment_gateway) {
-                    case 'paypal':
-                        return response(['redirect_url' => route('paypal.payment')]);
-                        break;
+                default:
+                    break;
+            }
+        }
+    }
 
-                    default:
-                        break;
-                }
-             }
-   }
 
-      /** Paypal Payment  */
+    /** Paypal Payment  */
 
-      function payWhitPaypal()
-      {
-        return 'Payment processing';
-      }
+    function setPaypalConfig(): array
+    {
+        $config = [
+            'mode'    => config('gatewaySettings.paypal_account_mode'), // Can only be 'sandbox' Or 'live'. If empty or invalid, 'live' will be used.
+            'sandbox' => [
+                'client_id'         => config('gatewaySettings.paypal_api_key'),
+                'client_secret'     => config('gatewaySettings.paypal_secret_key'),
+                'app_id'            => 'APP-80W284485P519543T',
+            ],
+            'live' => [
+                'client_id'         => config('gatewaySettings.paypal_api_key'),
+                'client_secret'     => config('gatewaySettings.paypal_secret_key'),
+                'app_id'            => env('PAYPAL_LIVE_APP_ID', ''),
+            ],
 
-      function paypalSuccess()
-      {
+            'payment_action' => 'Sale', // Can only be 'Sale', 'Authorization' or 'Order'
+            'currency'       => config('gatewaySettings.paypal_currency'),
+            'notify_url'     => env('PAYPAL_NOTIFY_URL', ''), // Change this accordingly for your application.
+            'locale'         => 'en_US', // force gateway language  i.e. it_IT, es_ES, en_US ... (for express checkout only)
+            'validate_ssl'   => true, // Validate SSL when creating api client.
+        ];
 
-      }
+        return $config;
+    }
 
-      function paypalCancel()
-       {
+    function payWhitPaypal()
+    {
+    }
 
-       }
+    function paypalSuccess()
+    {
+    }
+
+    function paypalCancel()
+    {
+    }
 }
